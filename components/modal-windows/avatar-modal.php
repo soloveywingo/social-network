db10127e52f447a496f30e9f818b9bba47842b3b
Bohdan Solovyov <solovey2wingo@mail.ru>
2018-05-18 14:25:16 +0300

    social network reborn

---
+++ components/modal-windows/avatar-modal.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LETO
+ * Date: 17.05.2018
+ * Time: 18:19
+ */
+
+?>
+
+<div class="bg-photo-modal">
+    <div class="modal">
+        <img src="img/new_photo.jpg" align="User photo">
+    </div>
+</div>
