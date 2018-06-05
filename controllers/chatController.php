517ecdd62d3a6f790d1d2edb59c65b9d919001ba
Bohdan Solovyov <solovey2wingo@mail.ru>
2018-06-05 21:17:07 +0300

    massage syst, fix them

---
+++ controllers/chatController.php
@@ -0,0 +1,17 @@
+<?
+
+$user = R::load('users2', $_SESSION['logged_user']->id);
+$data = $_POST;
+
+if (isset($data['massage_send']))
+{
+    $massage  = R::dispense('massages');
+    $massage->user_from = $_SESSION['logged_user']->id;
+    $massage->user_to = $_GET['sendTo'];
+    $massage->text = $data['massage_text'];
+    $massage->time = date("Y-m-d H:i:s");
+    R::store($massage);
+}
+
+
+?>
